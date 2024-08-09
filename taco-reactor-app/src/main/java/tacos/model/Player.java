8030317260a773fe3f77bc3844a06a59868d78a4
Utkarsh <ignition3951@gmail.com>
2024-08-09 21:14:41 +0530

    Implement Filter

---
+++ taco-reactor-app/src/main/java/tacos/model/Player.java
@@ -0,0 +1,13 @@
+package tacos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+
+	private String firstName;
+	private String lastName;
+
+}
--- taco-reactor-app/src/test/java/tacos/TacoReactorAppApplicationTests.java
+++ taco-reactor-app/src/test/java/tacos/TacoReactorAppApplicationTests.java
@@ -2,6 +2,7 @@ package tacos;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,8 +10,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
+import tacos.model.Player;
 
 @SpringBootTest
 class TacoReactorAppApplicationTests {
@@ -127,6 +131,32 @@ class TacoReactorAppApplicationTests {
 		Flux<String> nationalParkFlux = Flux.just("Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
 				.filter(np -> !np.contains(" "));
 		StepVerifier.create(nationalParkFlux).expectNext("Yellowstone", "Yosemite", "Zion").verifyComplete();
+	}
+
+	@Test
+	public void map() {
+		Flux<Player> playerFlux = Flux.just("Michael Jordan", "Scottie Pippen", "Steve Kerr").map(n -> {
+			String[] split = n.split("\\s");
+			return new Player(split[0], split[1]);
+		});
+
+		StepVerifier.create(playerFlux).expectNext(new Player("Michael", "Jordan"))
+				.expectNext(new Player("Scottie", "Pippen")).expectNext(new Player("Steve", "Kerr")).verifyComplete();
+	}
+
+	@Test
+	public void flatMap() {
+		Flux<Player> playerFlux = Flux.just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+				.flatMap(n -> Mono.just(n).map(p -> {
+					String[] split = p.split("\\s");
+					return new Player(split[0], split[1]);
+				}).subscribeOn(Schedulers.parallel()));
+
+		List<Player> playerList = Arrays.asList(new Player("Michael", "Jordan"), new Player("Scottie", "Pippen"),
+				new Player("Steve", "Kerr"));
+		StepVerifier.create(playerFlux).expectNextMatches(p -> playerList.contains(p))
+				.expectNextMatches(p -> playerList.contains(p)).expectNextMatches(p -> playerList.contains(p))
+				.verifyComplete();
 	}
 
 }
