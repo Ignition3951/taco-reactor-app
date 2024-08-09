515eb05f833af25149987f7a3e00ba53e36bc637
Utkarsh <ignition3951@gmail.com>
2024-08-09 17:49:49 +0530

    Implement fast and slow flux

--- taco-reactor-app/src/test/java/tacos/TacoReactorAppApplicationTests.java
+++ taco-reactor-app/src/test/java/tacos/TacoReactorAppApplicationTests.java
@@ -105,4 +105,15 @@ class TacoReactorAppApplicationTests {
 				.expectNext("Barbossa eats Apples").verifyComplete();
 	}
 
+	@Test
+	public void firstWithSignalFlux() {
+
+		Flux<String> slowFlux = Flux.just("tortoise", "snail", "sloth").delaySubscription(Duration.ofMillis(100));
+		Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+
+		Flux<String> firstFlux = Flux.firstWithSignal(slowFlux, fastFlux);
+
+		StepVerifier.create(firstFlux).expectNext("hare").expectNext("cheetah").expectNext("squirrel").verifyComplete();
+	}
+
 }
